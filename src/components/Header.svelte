83032b78b1f975480333339af6674a206926f2e1
Jakub Soboczyński <teziovsky@gmail.com>
2023-01-17 23:14:35 +0100

    feature/tez-84-implement-header-with-score-number

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<header
+	class="flex items-center justify-between w-full max-w-sm pl-6 pr-4 py-3 border-2 rounded-lg border-header-outline"
+>
+	<h1 class="flex uppercase leading-4 tracking-tight text-xl font-bold flex-col">
+		<span>rock</span><span>paper</span><span>scissors</span>
+	</h1>
+
+	<div class="flex flex-col items-center justify-center px-5 py-2 bg-white rounded-md text-grey">
+		<span class="text-sm text-score">score</span>
+		<span class="text-4xl font-bold text-dark">12</span>
+	</div>
+</header>
